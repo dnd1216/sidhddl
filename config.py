a7dea88e9e87a78c3554a857d5e05bcecf1c2c8a
DND <ruh2034@naver.com>
2019-08-04 22:31:10 +0900

    미니터 API

---
+++ config.py
@@ -0,0 +1,27 @@
+from sqlalchemy import create_engine, text
+
+db = {
+    'user' : 'dnd',
+    'password' : '12345678',
+    'host' : 'root',
+    'port' : 3306,
+    'database' : 'miniter'
+}
+
+DB_URL = f"mysql+mysqlconnector://{db['user']}:{db['password']}@{db['host']}:{db['port']}/{db['database']}?charset=utf8"
+
+JWT_SECRET_KEY = 'HELLO_MY_SECRET'
+
+
+
+## db_url = f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}?charset=utf8"
+##db = create_engine(db_url, encoding = 'utf-8', max_overflow = 0)
+
+
+##params = {'name' : '임나연'}
+##rows = db.execute(text("SELECT * FROM users WHERE name = :name"),params).fetchall()
+
+##for row in rows:
+    ##print(f"name : {row['name']}")
+    ##print(f"email : {row['email']}")
+    
